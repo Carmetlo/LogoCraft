12c1c4ae1f897fcfa6f38ef5cf85cb7a3a236eea
Michael Carmelo <michaelcarmelo22@gmail.com>
2023-11-29 18:50:17 -0700

    redirected files, added basic js format

---
+++ develop/index.js
@@ -0,0 +1,2 @@
+const inquirer = require('inquirer');
+const {Triangle, Cirle, Square} = require('./shape');
---
+++ develop/svg-logo-generator/lib/shapes.test.js
@@ -0,0 +1,13 @@
+const {Triangle, Square, Circle} = require('./shapes');
+
+Test('Triangle renders correcly', () => {
+    
+});
+
+test('Circle renders correcly', () => {
+
+});
+
+test('Square renders correcly', () => {
+
+});
