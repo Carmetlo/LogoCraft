14701e6bda98f06ae602e88b1e8e2b7ad2fe1ad8
Michael Carmelo <michaelcarmelo22@gmail.com>
2023-12-04 14:48:51 -0700

    reorganize files and added per tutor

---
+++ index.js
@@ -0,0 +1,62 @@
+const inquirer = require('inquirer');
+const {Triangle, Circle, Square} = require('./lib/shapes');
+const path = require ("path");
+const fs = require ("fs");
+
+async function getUserInput() {
+    const userInput = await inquirer.prompt([
+        {
+            type: "list",
+            name: "shapeType",
+            message: "Select your shape",
+            choices: [
+                "circle",
+                "square",
+                "triangle"
+            ]
+        },
+        {
+            type: "input",
+            name: "text",
+            message: "Enter 3 characters for your logo",
+        },
+        {
+            type: "input",
+            name: "textColor",
+            message: "Enter your text color",
+        },
+        {
+            type: "input",
+            name: "shapeColor",
+            message: "Enter your shape color",
+        }
+
+    ])
+     return userInput;
+}
+
+async function generateSVG(text, textColor, shapeType, shapeColor) {
+    let svgString = "";
+
+    if (shapeType === "circle") {
+        const circle = new Circle (text,textColor,shapeColor)
+        svgString = circle.render () + circle.insertShape () + circle.insertText () + circle.renderEnd ()
+    }
+    else if (shapeType === "square") {
+        const square = new Square (text,textColor,shapeColor)
+        svgString = square.render () + square.insertShape () + square.insertText () + square.renderEnd ()
+    }
+    else {
+        const triangle = new Triangle (text,textColor,shapeColor)
+        svgString = triangle.render () + triangle.insertShape () + triangle.insertText () + triangle.renderEnd ()
+    }
+    fs.writeFileSync(path.join (__dirname,"/examples/","logo.svg"),svgString)
+}
+
+async function run() {
+    const userInput = await getUserInput(); 
+    await generateSVG(userInput.text, userInput.textColor, userInput.shapeType, userInput.shapeColor);
+    console.log('SVG generated!');
+}
+
+run();
